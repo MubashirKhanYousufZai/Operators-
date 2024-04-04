787d942192dbda389c97e9c9065a186814b1cba7
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-04 16:08:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+// OPerators
+let num = 234 + 76 + 60;
+let num2 = 90 + 43 + 120;
+let result = (num + num2);
+console.log(result); //623
+let n = 7;
+let n2 = 1;
+console.log(n + n2); //8
+let sum = 23;
+let sum2 = 789;
+console.log(sum2 - sum); //766
+let ab = 121;
+let cd = 234;
+console.log(cd - ab); //113
+let number = 7;
+let number2 = 3;
+let answer = number * number2;
+console.log(answer); //21
+let o = 5 * 4; // 20
+let p = 6 * 2; //12
+console.log(o * p); // 240
+let k = 36;
+let l = 4;
+console.log(k / l);
+let y = 4;
+let z = 2;
+console.log(4 / 2);
